521da89e929dba58a1a0edb43af7642fd6fb7688
zhengguihao <guihao.zheng@rootcloud.com>
2024-01-15 18:55:11 +0800

    优化页面

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,24 +1,28 @@
 <script setup lang="ts">
+import logoUrl from '../assets/logo.png';
 </script>
 <template>
   <el-header class="page-header">
-    <el-image class="page-logo" src="https://upload.shejihz.com/2019/03/16dd1d1ec500dbccfcb52f210c4c1b07.jpg" fit="scale-down"/>
+    <el-image class="page-logo" :src="logoUrl" fit="scale-down"/>
     <div class="title">物流机器人管理系统</div>
     <div class="user-info">
-        <el-avatar :size="25" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png" />
-        <span>zhengguihao</span>
+        <label class="user-label">
+            <el-avatar :size="25" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png" />
+            <span>zhengguihao</span>
+        </label>
     </div>
   </el-header>
 </template>
 <style>
 .page-header {
-    background-color: #fff;
+    background-color: rgba(0, 255, 0, 0.1);
     padding: 0;
     position: relative;
 }
 
 .page-logo {
     width: 200px;
+    height: 100%;
     position: absolute;
     left: 0;
     top: 0;
@@ -39,8 +43,11 @@
     position: absolute;
     right: 2%;
     top: 25%;
+}
+.user-label {
     display: flex;
     align-items: center;
     justify-content: space-between;
+    cursor: pointer;
 }
 </style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 </script>
 <template>
-  <div>首页模块</div>
+  <div class="page-container">
+
+  </div>
 </template>
 <style scoped>
+.page-container {
+  background: url('../assets/data_bg.png');
+  background-size: cover;
+}
 </style>
