c4b2df859ed93fa2047d3801dde42a5542577cd8
zhengguihao <guihao.zheng@rootcloud.com>
2024-01-26 18:07:18 +0800

    个人中心

--- src/views/userControl.vue
+++ src/views/userControl.vue
@@ -1,14 +1,41 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
+import { ref } from 'vue';
 import Header from '@/components/header.vue';
+import MyBreadcrumb from '@/components/myBreadcrumb.vue';
+import { useUserStore } from '@/stores/user';
+
+const userStore = useUserStore()
 
 const route = useRoute()
 const userName = route.query.userName
+
+const activeName = ref('userInfo')
+
+const handleClick = () => {
+  
+}
 </script>
 <template>
   <el-container class="page-container">
     <Header></Header>
-    <el-main class="page-content"></el-main>
+    <el-main class="page-content">
+      <el-card class="user-card" body-style="width: 100%; height: 100%; display: flex; justify-content: center; align-items: center;">
+        <div class="user-avatar">
+          <el-avatar :size="100" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png" />
+          <el-text size="large">{{ userStore.userName }}</el-text>
+        </div>
+      </el-card>
+      <el-card class="user-card">
+        <template #header>
+          <MyBreadcrumb title="个人信息"></MyBreadcrumb>
+        </template>
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+          <el-tabs-pane label="基本信息" name="userInfo">基本信息</el-tabs-pane>
+          <el-tabs-pane label="修改密码" name="changePassword">修改密码</el-tabs-pane>
+        </el-tabs>
+      </el-card>
+    </el-main>
   </el-container>
 </template>
 <style scoped>
@@ -19,6 +46,17 @@ const userName = route.query.userName
   flex-direction: column;
 }
 .page-content {
-  background-color: aqua;
+  display: flex;
+  justify-content: space-between;
+}
+
+.user-card {
+  width: calc(45%);
+}
+
+.user-avatar {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
