09c73ca1f064ab6a21bfa87adfcb80bef9090f10
zhengguihao <gh475364602@gmail.com>
2024-05-08 15:27:29 +0800

    管理货物类型限制

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -20,9 +20,11 @@ const errorHandle = (error: any) => {
 }
 
 const successHandle = (response: any) => {
+    console.log('response', response)
     const data = response?.data
-    const { code, msg } = data
+    const { code, msg } = data as any
     if(code !== '200') {
+        message(msg, 'error')
         throw new Error('请求失败')
     } else {
         message(msg, 'success')
