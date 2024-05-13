994369eb7d3380e92ff732e3e02d628a68a63026
zhengguihao <gh475364602@gmail.com>
2024-05-13 18:38:12 +0800

    添加默认页

---
+++ src/views/components/Mapping/Default/index.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div class="default-container">
+        <el-card class="card">
+            <template #header>
+            <div class="card-header">
+                <span>物流方案一</span>
+            </div>
+            </template>
+            <p class="text">装载货物--装载机器人</p>
+            <p class="text">运输货物--带机械臂装载机器人</p>
+            <p class="text">上架货物--带机械臂装载机器人</p>
+        </el-card>
+        <el-card class="card">
+            <template #header>
+            <div class="card-header">
+                <span>物流方案二</span>
+            </div>
+            </template>
+            <p class="text">装载货物--装载机器人</p>
+            <p class="text">运输货物--装载机器人</p>
+            <p class="text">上架货物--机械臂</p>
+        </el-card>
+    </div>
+</template>
+<style scoped>
+.default-container {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+}
+.card {
+    min-width: 480px;
+    min-height: 500px;
+    margin: 20px 80px;
+}
+.text {
+    margin: 50px 10px;
+    font-size: 20px;
+    font-weight: bold;
+}
+</style>
--- src/views/components/Mapping/index.vue
+++ src/views/components/Mapping/index.vue
@@ -2,9 +2,10 @@
 import { ref, reactive, watch } from 'vue'
 import Scheme1 from './Scheme1/index.vue'
 import Scheme2 from './Scheme2/index.vue'
+import Default from './Default/index.vue'
 import type { GoodsType } from './type'
 
-const currentSchemeComponent = ref<typeof Scheme1 | typeof Scheme2 | null>(null)
+const currentSchemeComponent = ref<typeof Scheme1 | typeof Scheme2 | typeof Default | null>(null)
 
 const currentGoods = reactive<GoodsType>({
   goodsId: null,
@@ -17,8 +18,10 @@ watch(() => currentGoods.goodsId, (value) => {
     currentSchemeComponent.value = Scheme1
   } else if(currentGoods.goodsId === 2) {
     currentSchemeComponent.value = Scheme2
+  } else {
+    currentSchemeComponent.value = Default
   }
-})
+}, {immediate: true})
 
 </script>
 
@@ -26,7 +29,7 @@ watch(() => currentGoods.goodsId, (value) => {
   
   <div class="map-select">
     <span>请选择货物</span>
-    <el-select v-model="currentGoods.goodsId" placeholder="请选择货物" size="large">
+    <el-select v-model="currentGoods.goodsId" placeholder="请选择货物" size="large" clearable>
       <el-option label="货物1" :value="1"></el-option>
       <el-option label="货物2" :value="2"></el-option>
     </el-select>
